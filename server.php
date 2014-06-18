a49d3340cc883949890c0ab542616a44f42e9d93
sunil-kuna <sunilkuna5@gmail.com>
2014-06-19 05:13:52 +0530

    With updated ip

--- server.php
+++ server.php
@@ -59,6 +59,6 @@ $Server->bind('open', 'wsOnOpen');
 $Server->bind('close', 'wsOnClose');
 // for other computers to connect, you will probably need to change this to your LAN IP or external IP,
 // alternatively use: gethostbyaddr(gethostbyname($_SERVER['SERVER_NAME']))
-$Server->wsStartServer(gethostbyaddr(gethostbyname($_SERVER['SERVER_NAME'])),9300);
+$Server->wsStartServer("flirt-nahthing.rhcloud.com",80);
 
 ?>
